f24bbec61f64e9871d000dfaec6afd020a38fbf1
Weihe Chen <wc2582@columbia.edu>
2017-11-29 18:32:56 -0500

    web page template

--- takingBestBet/takingBestBet/settings.py
+++ takingBestBet/takingBestBet/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookies.apps.BookiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
