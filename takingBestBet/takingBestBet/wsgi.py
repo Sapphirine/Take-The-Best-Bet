9a71717393542ec761e2943536c4eb670847789d
Weihe Chen <wc2582@columbia.edu>
2017-11-28 20:20:20 -0500

    initializing project

--- takingBestBet/takingBestBet/wsgi.py
+++ takingBestBet/takingBestBet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for footballBookies project.
+WSGI config for takingBestBet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "footballBookies.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "takingBestBet.settings")
 
 application = get_wsgi_application()
