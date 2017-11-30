27325c991933adc1e3103829493c3f7f9d6a89c6
Weihe Chen <wc2582@columbia.edu>
2017-11-29 22:36:49 -0500

    odds frontend & backend interface

--- takingBestBet/bookies/urls.py
+++ takingBestBet/bookies/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-            url(r'^$', views.index, name='index'),
+        url(r'^$', views.index, name='index'),
+        url(r'^ajax/init/', views.init, name='init'),
+        url(r'^ajax/requestOdds/', views.requestOdds, name='requestOdds'),
         ]
--- takingBestBet/bookies/views.py
+++ takingBestBet/bookies/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'bookies/index.html')
+
+def init(request):
+    response = {"text": "place holder"}
+    return HttpResponse(json.dumps(response), content_type='application/json')
+
+def requestOdds(request):
+    response = {"text": "place holder"}
+    return HttpResponse(json.dumps(response), content_type='application/json')
