aa4b82060d012afddf38354028dad88192c2e660
Weihe Chen <wc2582@columbia.edu>
2017-12-13 22:11:57 -0500

    frontend changed and finished

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EECS6893_Big_Data_Analytics_Final_Project
+This is an app that helps people identify those odds with higher possible returns.
--- takingBestBet/bookies/views.py
+++ takingBestBet/bookies/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .odds import get_soccer_leagues, get_odds
-import json
+from operator import add
+import json, numpy
 
 # Create your views here.
 def index(request):
@@ -9,12 +10,35 @@ def index(request):
 
 def init(request):
     response = get_soccer_leagues()
-    print (json.dumps(response, indent=2))
+    #print (json.dumps(response, indent=2))
     return HttpResponse(json.dumps(response), content_type='application/json')
 
 def requestOdds(request):
+    print ("test")
     sport = request.GET['sport']
     region = request.GET['region']
     response = get_odds(sport, region)
-    #print (json.dumps(response, indent=2))
+    data = response['data']['events']
+    for i in data:
+        a = [0] * 3
+        cnt = 0
+        for j in data[i]['sites']:
+            odds = data[i]['sites'][j]['odds']['h2h']
+            if len(odds) < 3:
+                continue
+            cnt += 1
+            for k in range(3):
+                a[k] += float(odds[k])
+                print (k, a[k])
+        for k in range(3):
+            a[k] /= cnt
+            a[k] = 1/a[k]
+            if a[k] > 0.2:
+                a[k] *= 0.75
+            else:
+                a[k] *= 0.98
+            a[k] = 1/a[k]
+            a[k] = format(a[k], '.2f')
+        data[i]['p_odds'] = [str(x) for x in a]
     return HttpResponse(json.dumps(response), content_type='application/json')
+
